7b95a8a3cd3a95d8d52dbee70af4ba808dd59778
Xuong <xuong.trantrieu@gmail.com>
2019-08-21 16:33:32 +0700

    Add server started notice

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from tornado.ioloop import IOLoop
 from config import load_app
 
 if __name__ == '__main__':
+    print('Starting server...')
     load_app()
     main_loop = IOLoop.current()
     try:
+        def notice_start():
+            print('Server started!')
+        main_loop.spawn_callback(notice_start)
         main_loop.start()
     except KeyboardInterrupt:
+        print('\nServer stopped!')
         main_loop.stop()
