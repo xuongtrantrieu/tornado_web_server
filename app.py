a54f3d886695650b60aaf4c3bd38d81153529085
Xuong <xuong.trantrieu@gmail.com>
2019-08-21 10:58:51 +0700

    Prevent raising KeyboardInterrupt exception when stopping server with Ctrl+C

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ from config import load_app
 
 if __name__ == '__main__':
     load_app()
-    IOLoop.current().start()
+    main_loop = IOLoop.current()
+    try:
+        main_loop.start()
+    except KeyboardInterrupt:
+        main_loop.stop()
